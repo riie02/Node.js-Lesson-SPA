73f111eabed57f4607b7cc54f515d9a0bc0d245c
fuchiwaki_rie <0896.riri@gmail.com>
2021-03-17 15:54:57 +0900

    ユーザ一覧ページのユーザー一覧表示の実装

---
+++ client/src/views/pages/UserList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <my-navi />
+    <template>
+      <div
+        v-for="user in userList"
+        :key="user.id"
+      >
+        <p>id:{{ user.id }}</p>
+        <p>username:{{ user.username }}</p>
+      </div>
+    </template>
+  </div>
+
+</template>
+
+<script>
+import MyNavi from '@/components/Navi.vue';
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  created() {
+    this.getAllUser();
+  },
+  components: {
+    MyNavi
+  },
+  methods: {
+    ...mapActions({
+      getAllUser: 'updateUserList',
+    }),
+  },
+  computed: {
+    ...mapGetters([
+      'userList',
+    ])
+  },
+}
+</script>
